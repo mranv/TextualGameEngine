2627fd19597c5bbc9630f51e1d89e5ae8b276124
mranv <iamanubhavgain@gmail.com>
2024-06-12 18:32:14 +0530

    refactor: Add Component, ComponentManager, Entity, EntityManager, and SystemManager classes for managing game entities and components

---
+++ include/entity_manager.h
@@ -0,0 +1,19 @@
+#ifndef ENTITY_MANAGER_H
+#define ENTITY_MANAGER_H
+
+#include <vector>
+#include <string>
+#include "entity.h"
+
+class EntityManager {
+public:
+    Entity& createEntity(const std::string& name);
+    void destroyEntity(int id);
+    Entity* getEntity(int id);
+    const std::vector<Entity>& getEntities() const { return entities; }
+private:
+    std::vector<Entity> entities;
+    static int nextId;
+};
+
+#endif // ENTITY_MANAGER_H
